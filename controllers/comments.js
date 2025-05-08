194afee865aaeb79183118ad4345621782ceb8c5
Max Charles II <mcharles2016@yahoo.com>
2025-05-08 18:43:10 -0400

    Added like functionality

--- controllers/comments.js
+++ controllers/comments.js
@@ -43,32 +43,34 @@ module.exports = {
       console.log(err);
     }
   },
-//   likePost: async (req, res) => {
-//     try {
-//       await Post.findOneAndUpdate(
-//         { _id: req.params.id },
-//         {
-//           $inc: { likes: 1 },
-//         }
-//       );
-//       console.log("Likes +1");
-//       res.redirect(`/post/${req.params.id}`);
-//     } catch (err) {
-//       console.log(err);
-//     }
-//   },
-//   deletePost: async (req, res) => {
-//     try {
-//       // Find post by id
-//       let post = await Post.findById({ _id: req.params.id });
-//       // Delete image from cloudinary
-//       await cloudinary.uploader.destroy(post.cloudinaryId);
-//       // Delete post from db
-//       await Post.remove({ _id: req.params.id });
-//       console.log("Deleted Post");
-//       res.redirect("/profile");
-//     } catch (err) {
-//       res.redirect("/profile");
-//     }
-//   },
+  likeComment: async (req, res) => {
+    try {
+      const comment = await Comment.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+          $inc: { likes: 1 },
+        }
+      );
+      // console.log(comment);
+      console.log("Likes +1");
+      res.redirect(`/post/${comment.post}`);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  deleteComment: async (req, res) => {
+    try {
+      // Find post by id
+      let comment = await Comment.findById({ _id: req.params.id });
+      // Delete image from cloudinary
+      await cloudinary.uploader.destroy(post.cloudinaryId);
+      // Delete post from db
+      await Comment.remove({ _id: req.params.id });
+      console.log("Deleted Post");
+      res.redirect(`/post/${comment.post}`);
+    } catch (err) {
+      // res.redirect("/profile");
+      console.log(err);
+    }
+  },
 };
